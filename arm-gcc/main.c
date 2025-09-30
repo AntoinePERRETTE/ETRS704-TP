ae9e9deb82f3229cc42c4c6562d347667df04c4b
AntoinePERRETTE <antoineperrette71@ik.me>
2025-09-30 15:13:46 +0200

    Modified main.c

--- arm-gcc/main.c
+++ arm-gcc/main.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	// Enable Clock GPIOA 	(RCC->AHB1ENR)
-	// PA5 Output			(GPIOA->MODER)
+	Enable Clock GPIOA 	(RCC->AHB1ENR)
+	PA5 Output			(GPIOA->MODER)
 
 	while(1){
 		for(uint32_t i = 0 ; i<100000; i++);
-		// Toggle PA5		(GPIOA->ODR)	
+		Toggle PA5		(GPIOA->ODR)	
 	}
 }
