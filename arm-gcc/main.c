67bcb5affd89a149025b2adfc634f39af2d7bf4d
AntoinePERRETTE <antoineperrette71@ik.me>
2025-09-30 16:13:03 +0200

    Essai de compilation

--- arm-gcc/main.c
+++ arm-gcc/main.c
@@ -1,12 +1,14 @@
 #include <stdint.h>
+#include "stm32f446xx.h"
 
 int main(void)
 {
-	Enable Clock GPIOA 	(RCC->AHB1ENR)
-	PA5 Output			(GPIOA->MODER)
+
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	GPIOA->MODER |= 1 << 10;
 
 	while(1){
 		for(uint32_t i = 0 ; i<100000; i++);
-		Toggle PA5		(GPIOA->ODR)	
+		GPIOA->ODR ^= 1 << 5;	
 	}
 }
